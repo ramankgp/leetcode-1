cf5d8cf590e4388806abf4ce0758fb40a013b70d
Ren Zhang <ryanzjlib@gmail.com>
2020-05-15 15:37:00 -0400

    add 918

---
+++ code/918_solution.cpp
@@ -0,0 +1,36 @@
+// accum[i] = sum(A[:i])
+
+// accum[b]-accum[a] = sum(A[:b]) - sum(A[:a]) = sum(A[a:b])
+
+    
+// if sum(A[c:b]) is the maximum circular(strict) subarray sum 
+// then b = max(A) and c = min(A)
+
+    
+//          b
+//         /\
+//        /  \
+// s   /\/    \
+// \  /        \/\      e
+//  \/            \    /
+//   a             \  /
+//                  \/
+//                   c
+// ------------------------> i    
+
+
+
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& A) {
+        int total=0,curr_min=0,curr_max=0,min_sum=INT_MAX,max_sum=INT_MIN;
+        for (int num:A) {
+            total += num;
+            curr_max = max(curr_max+num, num);
+            curr_min = min(curr_min+num, num);
+            max_sum = max(max_sum, curr_max);
+            min_sum = min(min_sum, curr_min);
+        }
+        return max_sum < 0 ? max_sum : max(max_sum, total-min_sum);
+    }
+};
